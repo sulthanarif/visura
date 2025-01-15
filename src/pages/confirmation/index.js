c7d9dd3c0a99f54dc752e311b647d759578b9d63
unknown <msarizky201@gmail.com>
2025-01-15 09:02:42 +0700

    add react function page

---
+++ src/pages/confirmation/index.js
@@ -0,0 +1,18 @@
+// page confirmation
+import React from 'react';
+import DefaultLayout from '../../components/templates/DefaultLayout';
+import UploadBox from '../../components/organisms/UploadBox/index';
+import IconWithText from '@/components/molecules/IconWithText';
+
+
+function ConfirmationPage() {
+    return (
+        <DefaultLayout>
+            <div className="container">
+                <IconWithText icon="check-circle" text="This is a confirmation page" />
+            </div>
+         </DefaultLayout>
+    );
+}
+
+export default ConfirmationPage;
