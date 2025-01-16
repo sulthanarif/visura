f100387ec2ede76a907bbf88d9f8ab3a5d3e569d
Sulthan Arif <74910542+sulthanarif@users.noreply.github.com>
2025-01-16 07:40:01 +0700

    Update index.js

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,4 +1,4 @@
-// src/Components/auth/login.js
+// src/Components/login.js
 import React from "react";
 import LoginComponent from "../../components/auth/organism/LoginComponent";
 import AuthLayout from "../../components/auth/organism/AuthLayout";
