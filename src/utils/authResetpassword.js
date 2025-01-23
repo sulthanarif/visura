fa968b397ff884fa845686ddb0b583718aa5d740
FarellAlva <farell6646@gmail.com>
2025-01-23 11:13:16 +0700

    auth api config

---
+++ src/utils/authResetpassword.js
@@ -0,0 +1,32 @@
+// Fungsi untuk memverifikasi OTP
+export const verifyOtp = async ({ email, otp, password }) => {
+    try {
+      const response = await fetch("/api/auth/resetpassword", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, otp, password }),
+      });
+  
+      return await response.json();
+    } catch (error) {
+      console.error("Error verifying OTP:", error);
+      return { ok: false, message: "Terjadi kesalahan." };
+    }
+  };
+  
+  // Fungsi untuk mengirim ulang OTP
+  export const resendOtp = async ({ email }) => {
+    try {
+      const response = await fetch("/api/auth/otpresendresetpassword", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email }),
+      });
+  
+      return await response.json();
+    } catch (error) {
+      console.error("Error resending OTP:", error);
+      return { ok: false, message: "Gagal mengirim ulang OTP." };
+    }
+  };
+  
