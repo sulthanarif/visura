46b3d6a02f781c6944c3cb865379d49cc03e28a9
unknown <msarizky201@gmail.com>
2025-01-27 08:26:33 +0700

    add scan page

---
+++ src/pages/admin/library/scanfile/index.js
@@ -0,0 +1,15 @@
+// src/pages/upload-test/index.js
+
+import UploadBox from '../../../../components/organisms/UploadBox';
+
+
+const ScanFilePage =()=> {
+    return (
+            <div className="container">
+                <UploadBox/>
+            </div>
+         
+    );
+}
+
+export default ScanFilePage;
